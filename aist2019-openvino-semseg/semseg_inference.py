8b0e5591f45127de1960a074981d90e12766d687
Valentina <valentina-kustikova@users.noreply.github.com>
2019-04-21 18:14:58 +0300

    Fix formatting

--- aist2019-openvino-semseg/semseg_inference.py
+++ aist2019-openvino-semseg/semseg_inference.py
@@ -7,26 +7,26 @@ import logging as log
 from openvino.inference_engine import IENetwork, IEPlugin
 
 def build_argparser():
-    parser=argparse.ArgumentParser()
-    parser.add_argument('-m', '--model', help='Path to an .xml \
-        file with a trained model.', required=True, type=str)
-    parser.add_argument('-w', '--weights', help='Path to an .bin file \
-        with a trained weights.', required=True, type=str)
-    parser.add_argument('-i', '--input', help='Path to a folder with \
-        images or path to an image files', required=True, type=str, nargs='+')
-    parser.add_argument('-b', '--batch_size', help='Size of the  \
-        processed pack', default=1, type=int)
-    parser.add_argument('-l', '--cpu_extension', help='MKLDNN \
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-m', '--model', help = 'Path to an .xml \
+        file with a trained model.', required = True, type = str)
+    parser.add_argument('-w', '--weights', help = 'Path to an .bin file \
+        with a trained weights.', required = True, type = str)
+    parser.add_argument('-i', '--input', help = 'Path to a folder with \
+        images or path to an image files', required = True, type = str, nargs = '+')
+    parser.add_argument('-b', '--batch_size', help = 'Size of the  \
+        processed pack', default = 1, type = int)
+    parser.add_argument('-l', '--cpu_extension', help = 'MKLDNN \
         (CPU)-targeted custom layers.Absolute path to a shared library \
-        with the kernels implementation', type=str, default=None)
-    parser.add_argument('-pp', '--plugin_dir', help='Path to a plugin \
-        folder', type=str, default=None)
-    parser.add_argument('-d', '--device', help='Specify the target \
+        with the kernels implementation', type = str, default = None)
+    parser.add_argument('-pp', '--plugin_dir', help = 'Path to a plugin \
+        folder', type = str, default = None)
+    parser.add_argument('-d', '--device', help = 'Specify the target \
         device to infer on; CPU, GPU, FPGA or MYRIAD is acceptable. \
         Sample will look for a suitable plugin for device specified \
-        (CPU by default)', default='CPU', type=str)
-    parser.add_argument('--color_map', help='Classes color map', type=str,
-        default=None)
+        (CPU by default)', default = 'CPU', type = str)
+    parser.add_argument('--color_map', help = 'Classes color map', type = str,
+        default = None)
     return parser
 
 def prepare_model(model, weights, cpu_extension, device, plugin_dirs, log):
@@ -34,12 +34,12 @@ def prepare_model(model, weights, cpu_extension, device, plugin_dirs, log):
     model_bin = weights
 
     log.info('Creating plugin object for inference')
-    plugin = IEPlugin(device=device, plugin_dirs=plugin_dirs)
+    plugin = IEPlugin(device = device, plugin_dirs = plugin_dirs)
     if cpu_extension and 'CPU' in device:	
         plugin.add_cpu_extension(args.cpu_extension)
 
     log.info('Loading network files:\n\t{}\n\t{}'.format(model_xml, model_bin))
-    net = IENetwork(model=model_xml, weights=model_bin)
+    net = IENetwork(model = model_xml, weights = model_bin)
 
     log.info('Checking layers are supported by plugin')
     if 'CPU' in plugin.device:
@@ -67,7 +67,7 @@ def convert_image(net, data, log):
     log.info('Getting shape of input tensor')
     n, c, h, w = net.inputs[next(iter(net.inputs))].shape
     log.info('Creating input images tensor')
-    images = np.ndarray(shape=(len(data), c, h, w))
+    images = np.ndarray(shape = (len(data), c, h, w))
     for i in range(n):
         image = cv2.imread(data[i])
         if image.shape[:-1] != (h, w):
@@ -79,7 +79,7 @@ def convert_image(net, data, log):
 def infer_sync(images, exec_net, net):
     input_blob = next(iter(net.inputs))
     out_blob = next(iter(net.outputs))
-    res = exec_net.infer(inputs={input_blob : images})
+    res = exec_net.infer(inputs = {input_blob : images})
     res = res[out_blob]
     return res
 
@@ -97,7 +97,7 @@ def segmentation_output(res, color_map, log):
 
     log.info('Creating segmented images')
     for batch, data in enumerate(res):
-        classes_map = np.zeros(shape=(h, w, c), dtype=np.int)
+        classes_map = np.zeros(shape = (h, w, c), dtype = np.int)
         for i in range(h):
             for j in range(w):
                 if len(data[:, i, j]) == 1:
@@ -111,8 +111,8 @@ def segmentation_output(res, color_map, log):
         log.info('Result image was saved to {}'.format(out_img))
 
 def main():
-    log.basicConfig(format='[ %(levelname)s ] %(message)s',
-        level=log.INFO, stream=sys.stdout)
+    log.basicConfig(format = '[ %(levelname)s ] %(message)s',
+        level = log.INFO, stream = sys.stdout)
     args = build_argparser().parse_args()
     try:
         net, plugin = prepare_model(args.model, args.weights,
@@ -123,7 +123,7 @@ def main():
         images = convert_image(net, data, log)
 
         log.info('Loading model to the plugin')
-        exec_net = plugin.load(network=net)
+        exec_net = plugin.load(network = net)
 
         log.info('Starting inference')
         res = infer_sync(images, exec_net, net)

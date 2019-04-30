0ec317b39251733b3d4aa53b2d2effba286d7034
KchnKchn <khvatov.alexander@gmail.com>
2019-04-30 23:10:10 +0300

    code fixes

--- aist2019-openvino-semseg/semseg_inference.py
+++ aist2019-openvino-semseg/semseg_inference.py
@@ -4,29 +4,30 @@ import sys
 import argparse
 import numpy as np
 import logging as log
+from itertools import product
 from openvino.inference_engine import IENetwork, IEPlugin
 
 def build_argparser():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-m', '--model', help = 'Path to an .xml \
-        file with a trained model.', required = True, type = str)
-    parser.add_argument('-w', '--weights', help = 'Path to an .bin file \
-        with a trained weights.', required = True, type = str)
-    parser.add_argument('-i', '--input', help = 'Path to a folder with \
-        images or path to an image files', required = True, type = str, nargs = '+')
-    parser.add_argument('-b', '--batch_size', help = 'Size of the  \
-        processed pack', default = 1, type = int)
-    parser.add_argument('-l', '--cpu_extension', help = 'MKLDNN \
+    parser.add_argument('-m', '--model', help='Path to an .xml \
+        file with a trained model.', required=True, type=str)
+    parser.add_argument('-w', '--weights', help='Path to an .bin file \
+        with a trained weights.', required=True, type=str)
+    parser.add_argument('-i', '--input', help='Path to a folder with \
+        images or path to an image files', required=True, type=str, nargs='+')
+    parser.add_argument('-b', '--batch_size', help='Size of the  \
+        processed pack', default=1, type=int)
+    parser.add_argument('-l', '--cpu_extension', help='MKLDNN \
         (CPU)-targeted custom layers.Absolute path to a shared library \
-        with the kernels implementation', type = str, default = None)
-    parser.add_argument('-pp', '--plugin_dir', help = 'Path to a plugin \
-        folder', type = str, default = None)
-    parser.add_argument('-d', '--device', help = 'Specify the target \
+        with the kernels implementation', type=str, default=None)
+    parser.add_argument('-pp', '--plugin_dir', help='Path to a plugin \
+        folder', type=str, default=None)
+    parser.add_argument('-d', '--device', help='Specify the target \
         device to infer on; CPU, GPU, FPGA or MYRIAD is acceptable. \
         Sample will look for a suitable plugin for device specified \
-        (CPU by default)', default = 'CPU', type = str)
-    parser.add_argument('--color_map', help = 'Classes color map', type = str,
-        default = None)
+        (CPU by default)', default='CPU', type=str)
+    parser.add_argument('--color_map', help='Classes color map', type=str,
+        default=None)
     return parser
 
 def prepare_model(model, weights, cpu_extension, device, plugin_dirs, log):
@@ -34,19 +35,19 @@ def prepare_model(model, weights, cpu_extension, device, plugin_dirs, log):
     model_bin = weights
 
     log.info('Creating plugin object for inference')
-    plugin = IEPlugin(device = device, plugin_dirs = plugin_dirs)
+    plugin = IEPlugin(device=device, plugin_dirs=plugin_dirs)
     if cpu_extension and 'CPU' in device:	
-        plugin.add_cpu_extension(args.cpu_extension)
+        plugin.add_cpu_extension(cpu_extension)
 
     log.info('Loading network files:\n\t{}\n\t{}'.format(model_xml, model_bin))
-    net = IENetwork(model = model_xml, weights = model_bin)
+    net = IENetwork(model=model_xml, weights=model_bin)
 
     log.info('Checking layers are supported by plugin')
     if 'CPU' in plugin.device:
         supported_layers = plugin.get_supported_layers(net)
         not_supported_layers = [l for l in net.layers.keys() \
             if l not in supported_layers]
-        if len(not_supported_layers) != 0:
+        if not_supported_layers:
             log.error('Following layers are not supported by the plugin for specified' 
                 'device {}:\n {}'.format(plugin.device, ', '.join(not_supported_layers)))
             log.error('Please try to specify cpu extensions library path in sample\'s' 
@@ -67,7 +68,7 @@ def convert_image(net, data, log):
     log.info('Getting shape of input tensor')
     n, c, h, w = net.inputs[next(iter(net.inputs))].shape
     log.info('Creating input images tensor')
-    images = np.ndarray(shape = (len(data), c, h, w))
+    images = np.ndarray(shape=(len(data), c, h, w))
     for i in range(n):
         image = cv2.imread(data[i])
         if image.shape[:-1] != (h, w):
@@ -79,18 +80,16 @@ def convert_image(net, data, log):
 def infer_sync(images, exec_net, net):
     input_blob = next(iter(net.inputs))
     out_blob = next(iter(net.outputs))
-    res = exec_net.infer(inputs = {input_blob : images})
-    res = res[out_blob]
-    return res
+    res = exec_net.infer(inputs={input_blob : images})
+    return res[out_blob]
 
 def segmentation_output(res, color_map, log):
     c = 3
     h, w = res.shape[2:]
 
     log.info('Reading color map')
-    if not color_map:
-        color_map = 'color_map.txt'
-    classes_color_map=[]
+    color_map = color_map or 'color_map.txt'
+    classes_color_map = []
     with open(color_map, 'r') as f:
         for line in f:
             classes_color_map.append([int(x) for x in line.split()])
@@ -99,13 +98,12 @@ def segmentation_output(res, color_map, log):
     result_images = []
     for batch, data in enumerate(res):
         classes_map = np.zeros(shape=(h, w, c), dtype=np.int)
-        for i in range(h):
-            for j in range(w):
-                if len(data[:, i, j]) == 1:
-                    pixel_class = int(data[:, i, j])
-                else:
-                    pixel_class = np.argmax(data[:, i, j])
-                classes_map[i, j, :] = classes_color_map[min(pixel_class, 20)]
+        for i, j in product(range(h), range(w)):
+            if len(data[:, i, j]) == 1:
+                pixel_class = int(data[:, i, j])
+            else:
+                pixel_class = np.argmax(data[:, i, j])
+            classes_map[i, j, :] = classes_color_map[min(pixel_class, 20)]
         out_img = os.path.join(os.path.dirname(__file__), 
                     'out_segmentation_{}.bmp'.format(batch))
         result_images.append(out_img)
